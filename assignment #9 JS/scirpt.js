5c7444956205d8cedaf1a8ba2b96c4eec6ba9c5e
Muhammad Faizan <faizanyousaf815@gmail.com>
2023-02-20 13:34:04 +0500

    adding assignment #9 JS

---
+++ assignment #9 JS/scirpt.js
@@ -0,0 +1,78 @@
+// 1: 
+// let myVar =  5;
+// console.log(myVar);
+
+// // 2:
+// let myString = "Hello world";
+// alert(myString);
+
+// 3:
+let num1 = 5;
+let num2 = 10;
+let userNum = parseInt(prompt("Enter a number: " ));
+console.log(num1 + num2);
+console.log(userNum);
+
+// 4:
+// let myBoolean = true;
+// if (myBoolean == true) {
+//     console.log("It's true!");
+// }
+// else {
+//     console.log("It's false!")
+// }
+
+// 5:
+// let myNum = prompt();
+// if (myNum%2===0) {
+//     console.log("Even number!");
+// }
+// else{
+//     console.log("ODD number!");
+// }
+
+// 6:
+// let myStr1 = "Muhammad ";
+// let myStr2 = "Faizan";
+// let concat = myStr1+myStr2;
+// console.log(concat);
+
+// 7:
+// let num1 = 21;
+// let num2 = 2;
+// let mulitplyOP = num1*num2;
+// console.log(mulitplyOP);
+
+// 8:
+// let password  = "devfezi@";
+// let checkPass = prompt("Enter a password to confirm: ");
+// if (checkPass === password) {
+//     alert("ACCESS Granted!");
+// }
+// else {
+//     alert("ACCESS denied!");
+// }
+
+// 9:
+// let n1 = prompt("Enter 1st number: ");
+// let n2 = prompt("Enter 2nd number: ");
+// let n3 = prompt("Enter 3rd number: ");
+// if (n1>n2) {
+//     console.log(n1, " is the greatest number!");
+// }
+// else if (n2>n3) {
+//     console.log(n2, " is the greatest number!");
+// }
+// else {
+//     console.log(n3, "is the greatest number!")
+// }
+
+// 10:
+// let str1 = prompt("Enter the first string: ");
+// let str2 = prompt("Enter the second string: ");
+// if (str1 === str2) {
+//     alert("The strings are equal");
+// }
+// else {
+//     alert("The strings are not equal");
+// }
---
+++ assignment #9 JS/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let num1 = 5;
+let num2 = 10;
+let userNum = parseInt(prompt("Enter a number: "));
+console.log(num1 + num2);
+console.log(userNum);
